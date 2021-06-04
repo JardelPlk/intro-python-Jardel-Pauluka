39c431f1a4ff1781a72ea0cde61d2415cabc633d
JardelPlk <jardel.pauluka14@gmail.com>
2021-06-03 21:36:25 -0300

    Concluido o desafio-105.java.

--- desafio-105.java
+++ desafio-105.java
@@ -1,18 +1,18 @@
 /*#DESAFIO!!!
-#1) Crie uma lista com os 1000 primeiros numeros pares. Não use loop!
+#1) Crie uma lista com os 1000 primeiros numeros pares. NÃ£o use loop!
 #2) Crie uma lista com os numeros de 0 ate 99999999999999999999999999. Depois crie um loop para #percorrer a lista ateh encontrar o primeiro multiplo de 5.
 #OBS.: Use sua linguagem de programacao favorita. Nao use funcoes/metodos prontos.*/
 
 import java.util.ArrayList;
 
 public class Main {
-	
-    public static ArrayList<Integer> criarLista() {
-        ArrayList<Integer> listaPares = criarLista(0, new ArrayList<Integer>());
+
+    public static ArrayList<Double> criarLista() {
+        ArrayList<Double> listaPares = criarLista(0.0, new ArrayList<Double>());
         return listaPares;
     }
 
-    public static ArrayList<Integer> criarLista(int valor, ArrayList<Integer> listaPares) {
+    public static ArrayList<Double> criarLista(Double valor, ArrayList<Double> listaPares) {
         if (listaPares.size() < 1000) {
             listaPares.add(valor);
             listaPares = criarLista(valor + 2, listaPares);
@@ -25,18 +25,18 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ArrayList<Integer> listaPares = criarLista();
+		ArrayList<Double> listaPares = criarLista();
 
-        System.out.println("Tamanho da lista de números pares: " + listaPares.size());
+        System.out.println("Tamanho da lista de nÃºmeros pares: " + listaPares.size());
 
-        listaPares = new ArrayList<Integer>();
-        for (int indice = 0; indice < 99999999; indice++) {
+        listaPares = new ArrayList<Double>();
+        for (Double indice = 0.0; indice < 99999999999999999999999999.0; indice++) {
             listaPares.add(indice);
         }
 
-        for (int numero : listaPares) {
+        for (Double numero : listaPares) {
             if (numero % 5 == 0 && numero != 0 && numero != 5) {
-                System.out.println("Primeiro valor que é divisivel por 5 encontrado foi o " + numero);
+                System.out.println("Primeiro valor que Ã© divisivel por 5 encontrado foi o " + numero);
                 break;
             }
         }
